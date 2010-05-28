df3f9a39c99518df8226b2be346f85a538ff43d0
shuzhao.li <shuzhao.li@f64b4fb6-6e4f-0154-c2f3-24f0526c3d9d>
2010-05-28 18:50:59 +0000

    adding notes

---
+++ seaspider/__test__.py
@@ -0,0 +1,31 @@
+"""
+Unit test of SeaSpider,
+not implemented yet, as most functions 
+require extensive DB operation and IO.
+
+"""
+
+import unittest, spider3
+
+class TestSpider3Functions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
